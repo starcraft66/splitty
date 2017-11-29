82a71c1e9e15b852aa34406a65f9d7a707effa71
SOMEONELAPTOP\luxor <luxor37@gmail.com>
2017-11-29 09:26:03 -0500

    button main fixed2.0

--- app/src/main/java/co/tdude/splitty/AddEventActivity.java
+++ app/src/main/java/co/tdude/splitty/AddEventActivity.java
@@ -15,6 +15,7 @@ public class AddEventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
